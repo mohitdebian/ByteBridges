86216658120606e377abfab93f9fede95c0de1a1
Mohit || Dev <93594454+mohitdebian@users.noreply.github.com>
2024-02-05 22:21:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Define a JavaScript function to redirect to the specified URL
+function redirectToPeakWatch() {
+    window.location.href = "https://peakwatch.vercel.app/";
+}
+
+// Define a JavaScript function to redirect to the specified URL
+function redirectToGithub() {
+    window.location.href = "https://www.github.com/mohitdebian";
+}
